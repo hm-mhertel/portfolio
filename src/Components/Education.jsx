1031e181dbd13f40a39a7dbcfc7b50dca5c203a8
hm-mhertel <131671840+hm-mhertel@users.noreply.github.com>
2024-10-08 18:14:18 +0000

    added education 2

--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React from "../node_modules/react";
 
 const Education = () => {
-    return(
-        <section className="light" id="education">
-            <h2>Education</h2>
-        </section>
-    )
-}
+  return (
+    <section className="light" id="education">
+      <h2>Education</h2>
+    </section>
+  );
+};
 
 export default Education;
